2ba558975744547ca2baa5754241a581d4504689
Bernardngari <bernardngari.ke@gmail.com>
2022-06-25 01:46:41 +0300

    Add a functionality to display books fetched from API to the DOM

--- src/JS/project.js
+++ src/JS/project.js
@@ -29,3 +29,47 @@ function fetchAllBooks ( URL )
       
     } )
 }
+
+
+function displayBooks ( element )
+{
+  if ( element.price != '$0.00' )
+  {
+    let parent = document.createElement( 'div' )
+    parent.className='eachBook'
+    let display = document.querySelector( '.displayArea' )
+    let div1 = document.createElement( 'div' )
+    let img = document.createElement( 'img' )
+    img.Id = 'img'
+    img.src = element.image
+    div1.appendChild( img )
+    parent.appendChild( div1 )
+    let div2 = document.createElement( 'div' )
+    let div3 = document.createElement( 'div' )
+    let div4 = document.createElement( 'div' )
+    let div5 = document.createElement( 'div' )
+    div2.className = 'book'
+    div3.className = 'book'
+    div4.className = 'book'
+    div5.className = 'book'
+    let p = document.createElement( 'p' )
+    p.textContent = element.title
+    div2.appendChild( p )
+    parent.appendChild( div2 )
+    let p2 = document.createElement( 'p' )
+    p2.textContent = element.subtitle
+    div3.appendChild( p2 )
+    parent.appendChild( div3 )
+    let p3 = document.createElement( 'p' )
+    p3.textContent = element.price
+    div4.appendChild( p3 )
+    parent.appendChild( div4 )
+    let btn = document.createElement( 'button' )
+    btn.className = 'button'
+    btn.textContent = 'BUY NOW'
+    div5.appendChild( btn )
+    parent.appendChild( div5 )
+    display.appendChild(parent)
+
+  }   
+}
