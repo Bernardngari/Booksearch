27e66679bcc0b89ac22a171853e12919f4aa3d62
Bernardngari <bernardngari.ke@gmail.com>
2022-06-25 01:06:47 +0300

    Add a function that compiles user search and adds it to the API link as a suffix

---
+++ src/JS/project.js
@@ -0,0 +1,17 @@
+const searchForm = document.querySelector( '#form' )
+const input = document.querySelector( '#input' )
+const searchTerm = input.value
+
+
+function searchCompiler(){
+    searchForm.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const input = document.querySelector( '#input' )
+    const searchTerm = input.value
+    const URL= `https://api.itbook.store/1.0/search/${searchTerm}`
+    document.querySelector( '#displaySearchTerm' ).textContent = `You searched for books on ${ searchTerm.toUpperCase() }`
+    fetchAllBooks( URL )
+    searchForm.reset()
+    })
+    }
+    searchCompiler()
